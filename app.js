72851793602b2c50ae301addc11ba91127446d9d
henning-nr <henning.summer@novaroma.edu.br>
2022-10-22 08:53:23 -0300

    adjust route

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var http = require('http'); // 1 - Import Node.js core module
 var fs = require('fs');
-var server = http.createServer(function (req, res) {   // 2 - creating server
+var server = http.createServer(function async (req, res) {   // 2 - creating server
 
     var html = fs.readFileSync("./index.html", "utf8")
-    var resposta = html.replace('{ title }', 'Justiça <br> <a style="margin-right: -20px;">Eleitoral</a>'.toLocaleUpperCase());
+    var resposta = await html.replace('{ title }', 'Justiça <br> <a style="margin-right: -20px;">Eleitoral</a>'.toLocaleUpperCase());
     res.end(resposta)
 });
 
